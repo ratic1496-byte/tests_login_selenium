e891a5d3a09605b73f76f9ed7185795dfabb4276
Grigorii_QA <ratic14@mail.ru>
2026-05-16 20:52:30 +0300

    Initial commit for selenium tests

---
+++ test_form.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+def test_successful_login(driver):
+    driver.get("https://the-internet.herokuapp.com/login")
+    driver.find_element(By.ID, "username").send_keys("tomsmith")
+    driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    driver.find_element(By.CSS_SELECTOR, "button.radius").click()
+    assert "You logged into a secure area!" in driver.find_element(By.ID, "flash").text
+
+def test_unsuccessful_login(driver):
+    driver.get("https://the-internet.herokuapp.com/login")
+    driver.find_element(By.ID, "username").send_keys("user")
+    driver.find_element(By.ID, "password").send_keys("pass")
+    driver.find_element(By.CSS_SELECTOR, "button.radius").click()
+    assert "Your username is invalid!" in driver.find_element(By.ID, "flash").text
++
